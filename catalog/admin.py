3ae862958c1ef018a121c7b39161d0055d114ae2
VlZhelnov <vladimir.jelnov2015@yandex.ru>
2019-05-02 06:12:27 +0300

    debug

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
 #admin.site.register(Book)
-#admin.site.register(Author)
-#admin.site.register(Language)
+admin.site.register(Author)
+admin.site.register(Language)
 #admin.site.register(Genre)
 #admin.site.register(BookInstance)
 
